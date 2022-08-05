f26cd642c612e75f3f1d2df02a3491d91082fffd
Victor S. Miller <algebravic@fb.com>
2022-08-05 14:36:18 -0700

    Used not instead of xor, used csv for test data.

--- research-prototype/ipatest1.py
+++ research-prototype/ipatest1.py
@@ -5,11 +5,14 @@ import argparse
 import random
 from pathlib import Path
 import sys
+from csv import reader
 
 def get_args(args):
 
     parser = argparse.ArgumentParser(
         description = "Generate random IPA input data for MP-SPDZ")
+    parser.add_argument("data", type = str,
+                        help = "Name of data file")
     parser.add_argument("numrows_power", type = int, default = 12,
                         nargs = '?',
                         help = "Power of 2 of # of inputs")
@@ -19,17 +22,14 @@ def process(numrows_power: int,
             modulus: int = 2 ** 31,
             mkmod: int = 2 ** 8,
             breakdown_keys: int = 4):
-    head = [[3,1,11,0],
-            [3,0,0,1],
-            [2,1,7,0],
-            [2,0,0,2],
-            [4,0,0,3],
-            [4,1,8,0],
-            [4,1,6,0]
-            ]
+    lines = 0
+    with open(Path(fname + '.csv', 'r')) as fil:
+        for line in reader(fil):
+            lines += 1
+            yield ' '.join(line)
     numrows = 2 ** numrows_power
-    yield from head
-    yield from ([0,0,0,0] for _ in range(len(head), numrows))
+
+    yield from ([0,0,0,0] for _ in range(lines, numrows))
 
 def main():
 
