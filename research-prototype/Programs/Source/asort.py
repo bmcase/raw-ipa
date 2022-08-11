799f0cb5f1f6a2d22835d2dcd8238aab8c56247e
Victor S. Miller <algebravic@fb.com>
2022-08-11 10:42:06 -0700

    Fixed bug: x0 and x1 were interchanged.

--- research-prototype/Programs/Source/asort.py
+++ research-prototype/Programs/Source/asort.py
@@ -26,11 +26,12 @@ def dest_comp(B):
     @library.for_range(len(St_flat) - 1)
     def _(i):
         St_flat[i + 1] = St_flat[i + 1] + St_flat[i]
-    cumval = St_flat.get_vector(size = num)
-    cumshift = St_flat.get_vector(base = num, size = num) - cumval
-    dest = (cumval 
-            + (Bt_flat.get_vector(base = num, size = num)
-               * cumshift))
+    c_0 = St_flat.get_vector(size = num)
+    c_1 = St_flat.get_vector(base = num, size = num)
+    col = Bt_flat.get_vector(base = num, size = num)
+
+    dest = (c_0 + col * (c_1 - c_0))
+
     Tt = types.Array(num, B.value_type)
     Tt.assign_vector(dest - 1) # Make 0-origin
     return Tt
@@ -46,24 +47,42 @@ def double_dest(col0, col1):
     # col0 = bs.get_column(0)
     # col1 = bs.get_column(1)
     prod = col0 * col1
+    # (1 - x0) * (1 - x1)
     cum.assign_vector(prod - col0 - col1 + 1) # 00
-    cum.assign_vector(col1 - prod, base = num) # 01
-    cum.assign_vector(col0 - prod, base = 2 * num) # 10
+    # x0 * (1 - x1)
+    cum.assign_vector(col0 - prod, base = num) # 01
+    # x1 * (1 - x0)
+    cum.assign_vector(col1 - prod, base = 2 * num) # 10
+    # x0 * x1
     cum.assign_vector(prod, base = 3 * num) # 11
     @library.for_range(len(cum) - 1)
     def _(i):
         cum[i + 1] = cum[i + 1] + cum[i]
-    one_contrib = cum.get_vector(size = num)
-    col0_contrib = (cum.get_vector(base = 2 * num, size = num)
-                    - one_contrib)
-    col1_contrib = (cum.get_vector(base = num, size = num)
-                    - one_contrib)
-    prod_contrib = (one_contrib
-                    + cum.get_vector(base = 3 * num, size = num))
-    return (one_contrib
+    # (1 - x0) * (1 - x1) * c[00]
+    # + x0 * (1 - x1) * c[01]
+    # + (1 - x0) * x1 * c[10]
+    # + x0 * x1 * c[11]
+    # = c[00] + x0 * (-c[00] + c[01])
+    # + x1 * (- c[00] + c[10])
+    # + x0 * x1 * (c[00] - c[01] - c[10] + c[11])
+    # coefficients of 1
+    c00 = cum.get_vector(size = num)
+    c01 = cum.get_vector(base = num, size = num)
+    c10 = cum.get_vector(base = 2 * num, size = num)
+    c11 = cum.get_vector(base = 3 * num, size = num)
+    one_contrib = c00
+    # coefficient of col0
+    col0_contrib = c01 - c00
+    col1_contrib = c10 - c00
+    prod_contrib = c00 - c01 - c10 + c11
+
+    dest = (one_contrib
             + col0 * col0_contrib
             + col1 * col1_contrib
-            + prod * prod_contrib -1)
+            + prod * prod_contrib)
+    Tt = types.Array(num, type(col0))
+    Tt.assign_vector(dest - 1) # Make 0-origin
+    return Tt
     
 def double_bit_radix_sort(bs, D):
     """
@@ -92,7 +111,7 @@ def double_bit_radix_sort(bs, D):
         def _():
             @library.if_(n_bits % 2 == 1)
             def odd_case():
-                sorting.reveal_sort(h, bs[-1], reverse = True)
+                sorting.reveal_sort(h, bs[n_bits - 1], reverse = True)
                 c = types.Array.create_from(dest_comp(bs[-1]))
                 sorting.reveal_sort(c, h, reverse=False)
     # Now take care of the odd case
